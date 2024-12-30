6b146bb20fbb35278675992341d3826a973f2256
magno_xy <e.magnobn@gmail.com>
2024-12-30 08:30:15 -0400

    s

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express'; // Alteração para importação padrão
-
-const server = express(); // Agora, isso funciona corretamente com o express
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  await app.init();
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  console.log('Application is running on: http://localhost:3000/graphql');
 }
-
-bootstrap().then(() => {
-  module.exports = server;
-});
+bootstrap();
