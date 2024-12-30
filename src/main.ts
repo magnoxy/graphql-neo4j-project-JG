5d95707683bea7dc3668b4b1996a7f4daa5727c9
magno_xy <e.magnobn@gmail.com>
2024-12-30 08:12:54 -0400

    deploing

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import express from 'express'; // Alteração para importação padrão
 
-const server = express();
+const server = express(); // Agora, isso funciona corretamente com o express
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
