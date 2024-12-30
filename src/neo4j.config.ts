419f9097d9b35439958db2d2b69beeb0ed617944
magno_xy <e.magnobn@gmail.com>
2024-12-30 09:13:17 -0400

    pass.env

--- src/neo4j.config.ts
+++ src/neo4j.config.ts
@@ -1,9 +1,11 @@
 import neo4j from 'neo4j-driver';
 
 export const createDriver = () => {
-  const uri = process.env.NEO4J_URI || 'bolt://localhost:7687';
+  const uri =
+    process.env.NEO4J_URI || 'neo4j+s://79594acd.databases.neo4j.io:7687';
   const username = process.env.NEO4J_USERNAME || 'neo4j';
-  const password = process.env.NEO4J_PASSWORD || 'password';
+  const password =
+    process.env.NEO4J_PASSWORD || 'RGWDoWdns37gaKdNEgE5QepYz-PX4Bm4JrxhMRVkp_4';
 
   return neo4j.driver(uri, neo4j.auth.basic(username, password));
 };
